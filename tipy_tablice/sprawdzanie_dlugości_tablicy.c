b3d0fbbad5d55088e18e62371ac8e6fdb8c2708b
LWIPB <163990108+LWIPB@users.noreply.github.com>
2024-10-27 12:27:24 +0100

    Notatki kolejne

--- tipy_tablice/sprawdzanie_dlugości_tablicy.c
+++ tipy_tablice/sprawdzanie_dlugości_tablicy.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int tablica[] = {2147483647, 20, 30, 40, 50, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    // Deklaracja tablicy
+    // MAKSYMALNA WARTOŚĆ INT w C w pierwszej pozycji i jej odwrotność
+    int tablica[] = {2147483647, 20, 30, 40, 50, 1,1,1,1,1,1,1,1,1};
 
+    // Obliczenie długości tablicy
     int dlugosc = sizeof(tablica) / sizeof(tablica[0]);
     printf("Wartości w Bajtach! :)\n");
     printf("%d \n",sizeof(tablica));
--- zadanie_zajecia_trzecie.c
+++ zadanie_zajecia_trzecie.c
@@ -8,7 +8,12 @@ void print_menu_terminala() {
 
 
 int main() {
-    int size_of_list;
+    int size_of_list = {0};
+    int i;
+    printf("%d",size_of_list);
+    for (i = 0; i < 5; i++) {
+        printf("%d\n", i);
+    }
     // NIE DA się sprawdzić ROZMIARU TABLICY !!!!
     // bufor to lista/tuple/ciąg wartości w postaci STAŁEJ niezmiennej
     // bufor to wagonik dla wartości danej, którą wyciągamy i umiejscowiamy
