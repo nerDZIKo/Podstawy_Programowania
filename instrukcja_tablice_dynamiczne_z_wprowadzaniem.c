88673ef68deeb61a241db4437434e5463fff1d93
Jakub <97901102+nerDZIKo@users.noreply.github.com>
2024-11-11 21:36:10 +0100

    Update instrukcja_tablice_dynamiczne_z_wprowadzaniem.c

--- instrukcja_tablice_dynamiczne_z_wprowadzaniem.c
+++ instrukcja_tablice_dynamiczne_z_wprowadzaniem.c
@@ -4,24 +4,20 @@
 int main() {
     int size;
 
-    // Poproś użytkownika o podanie liczby wartości
     printf("Podaj liczbę wartości do wprowadzenia: ");
     scanf("%d", &size);
 
-    // Alokacja pamięci dla tablicy
     int *bufor = malloc(size * sizeof(int));
     if (bufor == NULL) {
         printf("Błąd alokacji pamięci\n");
         return 1;
     }
 
-    // Wprowadzenie wartości
     for (int i = 0; i < size; i++) {
         printf("Wprowadź wartość %d: ", i + 1);
         scanf("%d", &bufor[i]);
     }
 
-    // Wypisanie długości i zawartości tablicy
     printf("Długość tablicy: %d\n", size);
     printf("Zawartość tablicy: ");
     for (int i = 0; i < size; i++) {
@@ -29,6 +25,6 @@ int main() {
     }
     printf("\n");
 
-    free(bufor); // Zwolnienie pamięci
+    free(bufor); 
     return 0;
 }
