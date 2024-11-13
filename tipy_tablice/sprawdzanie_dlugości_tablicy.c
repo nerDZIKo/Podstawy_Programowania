07bd82bb10d6b83f8d816065a5b0719284576e6b
Jakub <97901102+nerDZIKo@users.noreply.github.com>
2024-11-13 18:11:38 +0100

    Update sprawdzanie_dlugości_tablicy.c

--- tipy_tablice/sprawdzanie_dlugości_tablicy.c
+++ tipy_tablice/sprawdzanie_dlugości_tablicy.c
@@ -3,7 +3,7 @@
 int main() {
     // Deklaracja tablicy
     // MAKSYMALNA WARTOŚĆ INT w C w pierwszej pozycji i jej odwrotność
-    int tablica[] = {2147483647, 20, 30, 40, 50, 1,1,1,1,1,1,1,1,1};
+    int tablica[] = {2147483647, 20, 30, 40, 50, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
     // Obliczenie długości tablicy
     int dlugosc = sizeof(tablica) / sizeof(tablica[0]);
